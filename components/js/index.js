5f4543a668a4939f17893efeb7ee4ee355f8dd19
Scott Newcomer <snewcomer24@gmail.com>
2015-01-14 17:52:22 -0600

    smooth scroll

--- components/js/index.js
+++ components/js/index.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   var wheight = $(window).height();
   var whalfheight = $(window).height()/2;
+  var topoffset = 95;
   $('.fullheight').css('height', wheight);
   $('.halfheight').css('height', whalfheight);
 
  ////Image Resizing///////////
   $(window).resize(function() {
-    var wheight = $(window).height(); 
+    var wheight = $(window).height();
     var whalfheight = $(window).height()/2;
     $('.fullheight').css('height', wheight);
       $('.halfheight').css('height', whalfheight);
@@ -15,14 +16,37 @@ $(document).ready(function() {
 
  ////Collapsing navbar////////
   $(document).on("scroll", function() {
-    if($(document).scrollTop() > 85) {
+    if($(document).scrollTop() > 100) {
       $("#navBar").removeClass("none").addClass("background");
-
       $('a').addClass('importantRule');
     } else {
       $("#navBar").removeClass("background").addClass("none");
       $('a').removeClass('importantRule');
     }
   });//collapsing navbar
+
+ ////////smooth scrolling///////////
+  $('a[href*=#]:not([href=#])').click(function() {
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+       var target = $(this.hash);
+       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+       if (target.length) {
+         $('html,body').animate({
+           scrollTop: target.offset().top - topoffset
+         }, 1000);
+         return false;
+       }
+     }
+   }); //ahref
+
+   $(window).scroll(function() {
+     var windowpos = $(window).scrollTop() + topoffset;
+     $('div.right a').removeClass('active');
+
+     if (windowpos > $('#aboutUs').offset().top) {
+       $('div.right a').removeClass('active');
+       $('a[href$="#aboutUs"]').addClass('active');
+     }
+   });
 
 });//document.ready
