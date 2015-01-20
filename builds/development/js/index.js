e07dad9a0c74185b47dec1b23de95d4ca1040f8c
Lucas Newcomer <newcomerlucas@gmail.com>
2015-01-19 21:13:54 -0600

    Changed the slideshow into a image fader
    I spent a lot of time trying to tweek the previous image slideshow i
    created to make so it would scroll for infinity and i could not figure
    out how to make it work. I commented out the previous slider work just
    in case you or I could figure it out. I decided though that this image
    fader was pretty cool and it infinitly loops through whatever pictures
    you put in the loop so I went with it.

--- builds/development/js/index.js
+++ builds/development/js/index.js
@@ -49,42 +49,84 @@ $(document).ready(function() {
        $('a[href$="#aboutUs"]').addClass('active');
      }
    });
-    
-    $(function(){
-        var scroller = $('#scroller div.innerScrollArea');
-        var scrollerContent = scroller.children('ul');
-        scrollerContent.children().clone().appendTo(scrollerContent);
-        var curX = 0;
-        scrollerContent.children().each(function(){
-            var $this = $(this);
-            $this.css('left', curX);
-            curX += $this.outerWidth(true);
-        });
-        var fullW = curX / 2;
-        var viewportW = scroller.width();
-
-        // Scrolling speed management
-        var controller = {curSpeed:0, fullSpeed:1};
-        var $controller = $(controller);
-        var tweenToNewSpeed = function(newSpeed, duration)
+        
+//    $(function(){
+//        var scroller = $('#scroller div.innerScrollArea');
+//        var scrollerContent = scroller.children('ul');
+//        scrollerContent.children().clone().appendTo(scrollerContent);
+//        var curX = 0;
+//        scrollerContent.children().each(function(){
+//            var $this = $(this);
+//            $this.css('left', curX);
+//            curX += $this.outerWidth(true);
+//        });
+//        var fullW = curX / 2;
+//        var viewportW = scroller.width();
+//
+//        // Scrolling speed management
+//        var controller = {curSpeed:0, fullSpeed:1};
+//        var $controller = $(controller);
+//        var tweenToNewSpeed = function(newSpeed, duration)
+//        {
+//            if (duration === undefined)
+//                duration = 600;
+//            $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+//        };
+//
+//        // Scrolling management; start the automatical scrolling
+//        var doScroll = function()
+//        {
+//            var curX = scroller.scrollLeft();
+//            var newX = curX + controller.curSpeed;
+//            if (newX > fullW*2 - viewportW)
+//                newX -= fullW;
+//            scroller.scrollLeft(newX);
+//        };
+//        setInterval(doScroll, 20);
+//        tweenToNewSpeed(controller.fullSpeed);
+//    });
+    $(window).load(function() { //start after HTML, images have loaded
+ 
+    var InfiniteRotator =
+    {
+        init: function()
         {
-            if (duration === undefined)
-                duration = 600;
-            $controller.stop(true).animate({curSpeed:newSpeed}, duration);
-        };
-
-        // Scrolling management; start the automatical scrolling
-        var doScroll = function()
-        {
-            var curX = scroller.scrollLeft();
-            var newX = curX + controller.curSpeed;
-            if (newX > fullW*2 - viewportW)
-                newX -= fullW;
-            scroller.scrollLeft(newX);
-        };
-        setInterval(doScroll, 20);
-        tweenToNewSpeed(controller.fullSpeed);
-    });
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 2500;
+ 
+            //interval between items (in milliseconds)
+            var itemInterval = 6000;
+ 
+            //cross-fade time (in milliseconds)
+            var fadeTime = 3000;
+ 
+            //count number of items
+            var numberOfItems = $('.rotating-item').length;
+ 
+            //set current item
+            var currentItem = 0;
+ 
+            //show first item
+            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+ 
+            //loop through the items
+            var infiniteLoop = setInterval(function(){
+                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+ 
+                if(currentItem == numberOfItems -1){
+                    currentItem = 0;
+                }else{
+                    currentItem++;
+                }
+                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+ 
+            }, itemInterval);
+        }
+    };
+ 
+    InfiniteRotator.init();
+ 
+});
 
 });//document.ready
 
